9ade26b0071e76a7d361a0cb142e5d2da57f1c68
Terranie Sims <terranie67@gmail.com>
2020-09-10 21:10:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,6 +3,12 @@ def loop_message_five_times (string)
 end
 
 def loop_message_n_times (string, integer)
+  counter = 0 
+  
+  while counter < integer do 
+    puts string[counter]
+    counter += 1 
+  end 
 end
 
 def output_array (array)
